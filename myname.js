c38115da8e5d6b96b7dbee76b4ff6f84c1f02e55
josephdeborah <josephdeborah873@gmail.com>
2024-12-10 16:44:09 -0800

    first commit

---
+++ myname.js
@@ -0,0 +1,9 @@
+alert("welcome")
+let firstName = prompt("Enter your first name")
+let lastName = prompt("Enter your last name")
+let age = prompt("Enter your age")
+
+
+let fullName = firstName  +" " +  lastName
+alert("Your full name is " + fullName + " and you are " + age + " years old" )
+x
